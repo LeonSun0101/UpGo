2591be5e077bab6cd1e951ebf39871bc96a89123
no0758 <dandan.l060@shulidata.com>
2018-10-21 19:06:27 +0800

    add senet module

--- layer_feature_map.py
+++ layer_feature_map.py
@@ -11,6 +11,7 @@ def get_layer_feature(layer,model,data_loader,shape):
     labels = []
     feature_data = {'features':features,'labels':labels}
     for batch_idx, (batch_x, batch_y) in enumerate(data_loader):
+        shape=(len(batch_x),shape[1],shape[2],shape[3])
         avgpool_layer = model._modules.get(layer)
         embedding = torch.zeros(shape)
         def fun(m, i, o): embedding.copy_(o.data)
@@ -27,20 +28,20 @@ def get_layer_feature(layer,model,data_loader,shape):
     return feature_data
 if __name__ == '__main__':
     #定义features保存文件
-    feature_save_file = 'valid_features.json'
+    feature_save_file = 'train_features.json'
     shape = (16,2048,1,1)
     num_classes = 61
 
     #加载model
     config = process_config(os.path.join(os.getcwd(), 'configs', 'config.json'))
     train_loader,valid_loader = get_data_loader(config)
-    model_path = config['test_model']
+    model_path = '/Users/lidandan/Documents/others_pro/model/val_eval_best.pth'
     model_param = torch.load(model_path) if torch.cuda.is_available() else torch.load(model_path,map_location='cpu')
     model = resnet50(num_classes=num_classes)
     model.load_state_dict(model_param)
 
     #获得指定layer的feature
-    feature_data = get_layer_feature(layer='avgpool',model=model,data_loader=valid_loader,shape=shape)
+    feature_data = get_layer_feature(layer='avgpool',model=model,data_loader=train_loader,shape=shape)
 
     #save
     json.dump(feature_data,open(os.getcwd()+feature_save_file))
